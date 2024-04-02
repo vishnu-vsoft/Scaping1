b55b6285de5c7f0122ad80cfdd0ad9ca51bbc646
vishnu-vsoft <vishnu.sudarsan@virtuosoft.in>
2024-04-02 14:20:42 +0530

    #merge

--- Scaping1/Controllers/ScrapingController.cs
+++ Scaping1/Controllers/ScrapingController.cs
@@ -5,6 +5,17 @@ using HtmlAgilityPack;
 using Microsoft.Azure.Batch.Protocol.Models;
 using PuppeteerSharp;
 using System.Reflection.Metadata;
+<<<<<<< Updated upstream
+=======
+using Aspose.Slides.Export.Web;
+using Aspose.Slides;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using Scaping1.Models;
+using Newtonsoft.Json;
+using System.Text.Json.Nodes;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+>>>>>>> Stashed changes
 
 namespace Scaping1.Controllers
 {
@@ -12,6 +23,10 @@ namespace Scaping1.Controllers
     [ApiController]
     public class ScrapingController : ControllerBase
     {
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
         [HttpPost]
         public async Task Awb(string id)
         {
@@ -21,7 +36,25 @@ namespace Scaping1.Controllers
                 Headless = false, // = false for testing
                 ExecutablePath = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
             };
+<<<<<<< Updated upstream
             
+=======
+            StatusHistory statusHistory = new StatusHistory();
+            AWBStatus awbstatus = new AWBStatus();
+            FlightDetail flightDetail = new FlightDetail();
+            var jsonObject = new
+            {
+                data = new
+                {
+                    StatusHistory = statusHistory,
+                    AWBStatus = awbstatus,
+                    FlightDetail = flightDetail
+                }
+            };
+
+
+            var jsonArray = new List<object>();
+>>>>>>> Stashed changes
 
             var browser = await Puppeteer.LaunchAsync(launchOptions);
             var page = await browser.NewPageAsync();
@@ -32,7 +65,7 @@ namespace Scaping1.Controllers
             string axbno = split.Last();
 
 
-            var pfix =  page.WaitForSelectorAsync("#txtPrefix").Result;
+            var pfix = page.WaitForSelectorAsync("#txtPrefix").Result;
             await pfix.TypeAsync(prefix);
 
             var ano = await page.WaitForSelectorAsync("#TextBoxAWBno");
@@ -44,6 +77,7 @@ namespace Scaping1.Controllers
             HtmlDocument document = new HtmlDocument();
             document.LoadHtml(htmlSource);
 
+<<<<<<< Updated upstream
             var nodes = document.DocumentNode.SelectNodes("//*[@id=\"gvBkAcInfo\"]");
             var table = nodes.Where(x=> x.FirstChild.Name == "#text" );
             //var nodes1 = document.DocumentNode.SelectNodes("//*[@id=\"GridViewAwbTracking\"]");
@@ -52,13 +86,115 @@ namespace Scaping1.Controllers
 
 
             if (nodes != null)
+=======
+
+            HtmlNode AWS = document.DocumentNode.SelectSingleNode("//*[@id=\"rptTrackAWBs_ctl00_btnAWBNumber\"]");
+            if (AWS != null)
+            {
+                awbstatus.AWB = AWS.Attributes["value"].Value;
+            }
+            //Console.WriteLine(status.LastActivity);
+            HtmlNode lastActvitydate = document.DocumentNode.SelectSingleNode("//*[@id=\"pnlMultipleAWBs\"]/div/div[2]/span");
+            awbstatus.LastActivity = lastActvitydate.InnerText.Trim();
+
+
+
+            var table1 = document.DocumentNode.SelectNodes("//*[@id=\"pnlShowData\"]/div[1]/table/tbody/tr/td[3]/table");
+            if (table1 != null)
+>>>>>>> Stashed changes
             {
                 foreach (var item in nodes)
                 {
+<<<<<<< Updated upstream
                     Console.WriteLine(item.InnerText);
+=======
+                    HtmlNode destination = row.SelectSingleNode(".//td[1]");
+                    statusHistory.Destination = destination.InnerText.Trim();
+                    awbstatus.Destination = destination.InnerText.Trim();
+>>>>>>> Stashed changes
                 }
 
             }
+<<<<<<< Updated upstream
+=======
+
+            var og = document.DocumentNode.SelectNodes("//*[@id=\"pnlShowData\"]/div[1]/table/tbody/tr/td[1]/table");
+            if (og != null)
+            {
+                foreach (var row in og)
+                {
+                    HtmlNode origin = row.SelectSingleNode(".//td[1]");
+                    statusHistory.Origin = origin.InnerText.Trim();
+                    awbstatus.Origin = origin.InnerText.Trim();
+                }
+            }
+
+
+            List<FlightDetail> flightDetailsList = new List<FlightDetail>();
+
+
+
+            
+
+            List<StatusHistory> statusHistories = new List<StatusHistory>();
+            var statusHist = document.DocumentNode.SelectNodes("//*[@id=\"GridViewAwbTracking\"]/tbody").FirstOrDefault()?.ChildNodes?.Where(x=>x.HasClass("newstyle-tr"));
+            if (statusHistory != null) {
+                foreach (var row in statusHist) {
+                    StatusHistory status = new StatusHistory();
+                    status.Station = row.ChildNodes[1].InnerText.Trim();
+                    status.MileStone = row.ChildNodes[2].InnerText.Trim();
+                    status.Pcs = row.ChildNodes[3].InnerText.Trim();
+                    status.Weight = row.ChildNodes[4].InnerText.Trim();
+                    status.FlightNo = row.ChildNodes[5].InnerText.Trim();
+                    status.FlightDate = row.ChildNodes[6].InnerText.Trim();
+                    status.Origin = row.ChildNodes[7].InnerText.Trim();
+                    status.Destination = row.ChildNodes[8].InnerText.Trim();
+                    status.ULD = row.ChildNodes[9].InnerText.Trim();  
+
+                    statusHistories.Add(status);    
+
+                    Console.WriteLine(statusHistories);
+
+                }
+               
+            }
+
+            var bookingAndAcceptance = document.DocumentNode.SelectNodes("//*[@id=\"gvBkAcInfo\"]/tbody").FirstOrDefault()?.ChildNodes?.Where(x => x.HasClass("newstyle-tr"));
+            //*[@id="gvBkAcInfo"]/tbody
+            List<FlightDetail> flightDetails = new List<FlightDetail>();
+            if (bookingAndAcceptance != null)
+            {
+                foreach (var row in bookingAndAcceptance)
+                {
+                    FlightDetail flightDetail1 = new FlightDetail();
+                    var sampleNumber = row.ChildNodes[6].InnerText;
+                    string[] aboveSplit = sampleNumber.Split(' ');
+                    flightDetail1.Number = aboveSplit.First();
+                    flightDetail.Date = aboveSplit.Last();
+                    flightDetail1.Origin = row.ChildNodes[2].InnerText.Trim();
+                    flightDetail1.Destination = row.ChildNodes[3].InnerText.Trim();
+                    flightDetails.Add(flightDetail1 );
+                }
+            }
+
+            List<FlightSchedules> flights = new List<FlightSchedules>();
+            FlightSchedules flightSchedules = new FlightSchedules();
+            string flightNumber = flightDetails.Select(x=>x.Number).First();
+            flightSchedules.FlightNo = flightNumber;
+            flightSchedules.AirlineCode = flightNumber.Substring(0, 2);
+            flightSchedules.Station = flightDetails.Select(x=>x.Origin).First();
+            flightSchedules.Destination = flightDetails.Select(x=>x.Destination).First();
+            flightSchedules.ETA = statusHistories.Where(x => x.MileStone == "Arrived").Select(x => x.FlightDate).First();
+            flightSchedules.ETD = statusHistories.Where(x => x.MileStone == "Departed").Select(x => x.FlightDate).First();
+            
+
+
+            string json = JsonConvert.SerializeObject(jsonArray, Formatting.Indented);
+            Console.WriteLine(json);
+>>>>>>> Stashed changes
         }
     }
 }
+        
+    
+
